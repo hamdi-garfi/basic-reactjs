6c1400b3286303f30eb7dc130b15ae1cd70722df
garfi.hamdi@gmail.com <garfi.hamdi@gmail.com>
2021-02-18 15:40:38 +0100

    Add scripts start application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
 
-class App extends Component{
-   render(){
-      return(
-         <div>
-            <h1>Hello World</h1>
-         </div>
-      );
-   }
-}
-export default App;
+ReactDOM.render(<App />, document.getElementById('app'));
